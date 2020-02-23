9139fcc2ec810cd20ae0c6fa8b132abe6ffa9a43
Fierflame <git@fierflame.com>
2020-02-23 12:56:10 +0800

    feat: 完善 install

--- src/core/install.ts
+++ src/core/install.ts
@@ -3,36 +3,45 @@ import { IRender } from './type';
 
 export let monitorable: typeof monitorableApi;
 
+export interface InstallOptions {
+	monitorable?: typeof monitorableApi;
+	render?: IRender;
+	renders?: IRender[];
+	devtools?: any;
+}
+
 export const renders: Record<string, IRender>
 	= Object.create(null);
-export function getRender(
+
+	export function getRender(
 	type: string | number | IRender = ''
 	): IRender {
 	if (typeof type === 'object') { return type; }
 	return renders[type] || renders.default;
 }
-export default function install(apis: {
-	monitorable?: typeof monitorableApi;
-	render?: IRender;
-	renders?: IRender[];
-}) {
-	const { render } = apis;
+
+function installRender({ render, renders: list}: InstallOptions) {
 	if (render) {
 		renders[render.type] = render;
 		if (!renders.default) {
 			renders.default = render;
 		}
 	}
-	if (Array.isArray(apis.renders)) {
-		for (const render of apis.renders) {
+	if (Array.isArray(list)) {
+		for (const render of list) {
 			renders[render.type] = render;
 		}
 		if (!renders.default) {
-			const [render] = apis.renders;
+			const [render] = list;
 			if (render) { renders.default = render; }
 		}
 	}
+}
+
+
+export default function install(apis: InstallOptions) {
 	if (apis.monitorable) {
 		monitorable = apis.monitorable;
 	}
+	installRender(apis);
 }
