0e18dfe4ed0114ea2b1e5c286909b06126ebdc4a
Fierflame <git@fierflame.com>
2020-10-13 22:54:33 +0800

    feat: 完善样式处理，以支持 Value，并增加单位处理

--- packages/web-render/src/update/style.ts
+++ packages/web-render/src/update/style.ts
@@ -1,26 +1,64 @@
 import { isValue } from '../install/neep';
 
-export type Style = string | Record<string, [string, 'important' | null]>;
+export type Style = string | Record<string, [string, 'important' | undefined]>;
 
+const unit: Record<string, string> = {
+	'width': 'px',
+	'height': 'px',
+	'top': 'px',
+	'right': 'px',
+	'bottom': 'px',
+	'left': 'px',
+	'border': 'px',
+	'border-top': 'px',
+	'border-right': 'px',
+	'border-left': 'px',
+	'border-bottom': 'px',
+	'border-width': 'px',
+	'border-top-width': 'px',
+	'border-right-width': 'px',
+	'border-left-width': 'px',
+	'border-bottom-width': 'px',
+	'border-radius': 'px',
+	'border-top-left-radius': 'px',
+	'border-top-right-radius': 'px',
+	'border-bottom-left-radius': 'px',
+	'border-bottom-right-radius': 'px',
+	'padding': 'px',
+	'padding-top': 'px',
+	'padding-right': 'px',
+	'padding-left': 'px',
+	'padding-bottom': 'px',
+	'margin': 'px',
+	'margin-top': 'px',
+	'margin-right': 'px',
+	'margin-left': 'px',
+	'margin-bottom': 'px',
+};
 function getStyle(
 	style: any,
 ): Style | undefined {
+	if (isValue(style)) { style = style(); }
 	if (typeof style === 'string') { return style; }
 	if (!style) { return undefined; }
 	if (typeof style !== 'object') { return undefined; }
-	const css: Record<string, [string, 'important' | null]> =
+	const css: Record<string, [string, 'important' | undefined]> =
 		Object.create(null);
 	for (let k in style) {
 		let value = style[k];
+		if (isValue(value)) { value = value(); }
 		const key = k.substr(0, 2) === '--' ? k
 			: k.replace(/[A-Z]/g, '-$1')
 				.replace(/-+/g, '-')
 				.toLowerCase();
 		if (typeof value === 'number') {
-			css[key] = [value === 0 ? '0' : `${ value }px`, null];
+			const str = value && k in unit
+				? `${ value }${ unit[k] }`
+				: `${ value }`;
+			css[key] = [str, undefined];
 		} else if (value && typeof value === 'string') {
 			const v = value.replace(/!important\s*$/, '');
-			css[key] = [v, v === value ? null : 'important'];
+			css[key] = [v, v === value ? undefined : 'important'];
 		}
 	}
 	return css;
@@ -83,7 +121,9 @@ export default function updateStyle(
 ): Style | undefined {
 	if (!hasStyle) { return undefined; }
 
-	const style = getStyle(isValue(props.style) ? props.style() : props.style);
+	const style = getStyle(
+		isValue(props.style) ? props.style() : props.style,
+	);
 	update(css, style, old);
 	return style;
 }
